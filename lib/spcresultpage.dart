339a348cb133d38469a4bb5a4c710b562480baaa
Tanvi <tanvichavan.2113@gmail.com>
2023-12-12 19:26:06 +0530

    added updated code that calculate and insert and also display on applications

--- lib/spcresultpage.dart
+++ lib/spcresultpage.dart
@@ -1,169 +1,3 @@
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class SPCResultPage extends StatefulWidget {
-//   const SPCResultPage({super.key});
-
-//   @override
-//   State<SPCResultPage> createState() => _SPCResultPageState();
-// }
-
-// class _SPCResultPageState extends State<SPCResultPage> {
-//   get selectedSampleSize => null;
-
-//   String? selectData = "";
-
-//   Future<List<dynamic>> fetchData(String sampleSize) async {
-//     final response = await http.post(
-//       Uri.parse('http://localhost:3001/calculate'),
-//       headers: {'Content-Type': 'application/json'},
-//       body: jsonEncode({'sampleSize': sampleSize}),
-//     );
-
-//     if (response.statusCode == 200) {
-//       return jsonDecode(response.body);
-//     } else {
-//       throw Exception('Failed to load data');
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     _getData();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('SPC Result App'),
-//       ),
-//       body: FutureBuilder(
-//         future: fetchData(selectData ?? ''), // Pass the selected sample size
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return const Center(child: CircularProgressIndicator());
-//           } else if (snapshot.hasError) {
-//             return Center(child: Text('Error: ${snapshot.error}'));
-//           } else {
-//             // Display your data using snapshot.data
-//             List<dynamic> data = snapshot.data as List<dynamic>;
-
-//             return ListView.builder(
-//               itemCount: 1,
-//               itemBuilder: (context, index) {
-//                 return ListTile(
-//                   title: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text('X bar: ${data[index]['xbar']}'),
-//                       Text('Stdev Overall ${data[index]['sd']}'),
-//                       Text('Pp: ${data[index]['pp']}'),
-//                       Text('Ppu: ${data[index]['ppu']}'),
-//                       Text('Ppl: ${data[index]['ppl']}'),
-//                       Text('Ppk: ${data[index]['ppk']}'),
-//                       Text('Rbar: ${data[index]['rbar']}'),
-//                       Text('Stdev Within: ${data[index]['sdw']}'),
-//                       Text('Cp: ${data[index]['cp']}'),
-//                       Text('Cpu: ${data[index]['cpu']}'),
-//                       Text('Cpl: ${data[index]['cpl']}'),
-//                       Text('Cpk: ${data[index]['cpk']}'),
-//                       Text('ucl: ${data[index]['ucl']}'),
-//                       Text('lcl: ${data[index]['lcl']}'),
-//                     ],
-//                   ),
-//                 );
-//               },
-//             );
-//           }
-//         },
-//       ),
-//     );
-//   }
-
-//   _getData() async {
-//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-//     selectData = sharedPreferences.getString("spc_option") ?? "0";
-//     print("Selected Data : $selectData");
-//     setState(() {});
-//   }
-// }
-
-// class SPCResultPage extends StatelessWidget {
-//   const SPCResultPage({Key? key});
-
-//   get selectedSampleSize => null;
-
-//   Future<List<dynamic>> fetchData(String sampleSize) async {
-//     final res = await http.post(
-//       Uri.parse('http://localhost:3001/calculate-spc'),
-//       headers: {'Content-Type': 'application/json'},
-//       body: jsonEncode({'sampleSize': sampleSize}),
-//     );
-
-//     if (res.statusCode == 200) {
-//       return jsonDecode(res.body);
-//     } else {
-//       throw Exception('Failed to load data');
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return
-//      Scaffold(
-//       appBar: AppBar(
-//         title: const Text('SPC Result App'),
-//       ),
-//       body: FutureBuilder(
-//         future: fetchData(
-//             selectedSampleSize ?? ''), // Pass the selected sample size
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return const Center(child: CircularProgressIndicator());
-//           } else if (snapshot.hasError) {
-//             return Center(child: Text('Error: ${snapshot.error}'));
-//           } else {
-//             // Display your data using snapshot.data
-//             List<dynamic> data = snapshot.data as List<dynamic>;
-
-//             return ListView.builder(
-//               itemCount: 1,
-//               itemBuilder: (context, index) {
-//                 return ListTile(
-//                   title: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text('X bar: ${data[index]['xbar']}'),
-//                       Text('Stdev Overall ${data[index]['sd']}'),
-//                       Text('Pp: ${data[index]['pp']}'),
-//                       Text('Ppu: ${data[index]['ppu']}'),
-//                       Text('Ppl: ${data[index]['ppl']}'),
-//                       Text('Ppk: ${data[index]['ppk']}'),
-//                       Text('Rbar: ${data[index]['rbar']}'),
-//                       Text('Stdev Within: ${data[index]['sdw']}'),
-//                       Text('Cp: ${data[index]['cp']}'),
-//                       Text('Cpu: ${data[index]['cpu']}'),
-//                       Text('Cpl: ${data[index]['cpl']}'),
-//                       Text('Cpk: ${data[index]['cpk']}'),
-//                       Text('ucl: ${data[index]['ucl']}'),
-//                       Text('lcl: ${data[index]['lcl']}'),
-//                     ],
-//                   ),
-//                 );
-//               },
-//             );
-//           }
-//         },
-//       ),
-//     );
-//   }
-// }
-
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -179,17 +13,28 @@ class SPCResultPage extends StatefulWidget {
 class _SPCResultPageState extends State<SPCResultPage> {
   String? selectedOption = "";
 
-  Future<List<dynamic>> fetchData(String selectedOption) async {
-    final response = await http.post(
-      Uri.parse('http://localhost:3001/calculate'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'selectedOption': selectedOption}),
-    );
+  Future<List<Map<String, dynamic>>> fetchData(String selectedOption) async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://localhost:3001/calculate'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'selectedOption': selectedOption}),
+      );
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to load data');
+      print('API Response: ${response.body}');
+
+      if (response.statusCode == 200) {
+        return [jsonDecode(response.body)];
+      } else {
+        return [
+          {'error': 'Failed to load data'}
+        ];
+      }
+    } catch (error) {
+      print('Exception: $error');
+      return [
+        {'error': 'An error occurred during data fetching'}
+      ];
     }
   }
 
@@ -213,31 +58,33 @@ class _SPCResultPageState extends State<SPCResultPage> {
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            List<dynamic> data = snapshot.data as List<dynamic>;
+            List<Map<String, dynamic>> data =
+                snapshot.data as List<Map<String, dynamic>>;
+            // print('Type of data: ${snapshot.data.runtimeType}');
 
             return ListView.builder(
-              itemCount: 1,
+              itemCount: data.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('X bar: ${data[index]['xbar']}'),
-                      Text('Stdev Overall ${data[index]['sd']}'),
-                      Text('Pp: ${data[index]['pp']}'),
-                      Text('Ppu: ${data[index]['ppu']}'),
-                      Text('Ppl: ${data[index]['ppl']}'),
-                      Text('Ppk: ${data[index]['ppk']}'),
-                      Text('Rbar: ${data[index]['rbar']}'),
-                      Text('Stdev Within: ${data[index]['sdw']}'),
-                      Text('Cp: ${data[index]['cp']}'),
-                      Text('Cpu: ${data[index]['cpu']}'),
-                      Text('Cpl: ${data[index]['cpl']}'),
-                      Text('Cpk: ${data[index]['cpk']}'),
-                      Text('ucl: ${data[index]['ucl']}'),
-                      Text('lcl: ${data[index]['lcl']}'),
-                    ],
-                  ),
+                Map<String, dynamic> result = data[index];
+                print('Result for index $index: $result');
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('X bar: ${result['xbar'].toString()}'),
+                    Text('Stdev Overall: ${result['sd'].toString()}'),
+                    Text('Pp: ${result['pp'].toString()}'),
+                    Text('Ppu: ${result['ppu'].toString()}'),
+                    Text('Ppl: ${result['ppl'].toString()}'),
+                    Text('Ppk: ${result['ppk'].toString()}'),
+                    Text('Rbar: ${result['rbar'].toString()}'),
+                    Text('Stdev Within: ${result['sdw'].toString()}'),
+                    Text('Cp: ${result['cp'].toString()}'),
+                    Text('Cpu: ${result['cpu'].toString()}'),
+                    Text('Cpl: ${result['cpl'].toString()}'),
+                    Text('Cpk: ${result['cpk'].toString()}'),
+                    Text('ucl: ${result['ucl'].toString()}'),
+                    Text('lcl: ${result['lcl'].toString()}'),
+                  ],
                 );
               },
             );
